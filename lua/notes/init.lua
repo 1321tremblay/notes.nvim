6ade47e18cbecf199e40b2577283ff47f1d76697
Olivier Tremblay <olivier.tremlay.gh@outlook.com>
2025-02-23 14:03:04 -0500

    debug

--- lua/notes/init.lua
+++ lua/notes/init.lua
@@ -3,67 +3,72 @@ local previous_position = {}
 local M = {}
 
 M.config = {
-	notes_dir = "$HOME/notes.nvim",
+  notes_dir = "$HOME/notes.nvim",
 }
 
-M.setup = function(opts)
-	M.config = vim.tbl_extend("force", M.config, opts or {})
-	-- nothing
+function M.setup(opts)
+  -- Add debug print statements to confirm the input and output
+  print("Default config:", vim.inspect(M.config))
+  print("User opts:", vim.inspect(opts))
+
+  M.config = vim.tbl_extend("force", M.config, opts or {})
+
+  -- Print the merged config to check if the merge worked as expected
+  print("Final config:", vim.inspect(M.config))
 end
 
 function M.OpenNotes()
-	local current_buf = vim.api.nvim_get_current_buf()
-	local current_win = vim.api.nvim_get_current_win()
-	local current_cursor = vim.api.nvim_win_get_cursor(current_win)
+  local current_buf = vim.api.nvim_get_current_buf()
+  local current_win = vim.api.nvim_get_current_win()
+  local current_cursor = vim.api.nvim_win_get_cursor(current_win)
 
-	previous_position = {
-		buf = current_buf,
-		win = current_win,
-		cursor = current_cursor,
-	}
+  previous_position = {
+    buf = current_buf,
+    win = current_win,
+    cursor = current_cursor,
+  }
 
-	local oil_ok, oil = pcall(require, "oil")
-	if M.config.notes_dir then
-		local notes_dir = vim.fn.expand(M.config.notes_dir)
-		print("Open Notes")
-		if oil_ok then
-			oil.open(notes_dir)
-		else
-			vim.cmd("Ex " .. notes_dir)
-			return
-		end
-	end
+  local oil_ok, oil = pcall(require, "oil")
+  if M.config.notes_dir then
+    local notes_dir = vim.fn.expand(M.config.notes_dir)
+    if oil_ok then
+      oil.open(notes_dir)
+    else
+      vim.cmd("Ex " .. notes_dir)
+      return
+    end
+  end
 end
 
 function M.CloseNotes()
-	local current_working_dir = vim.fn.getcwd()
+  local current_working_dir = vim.fn.getcwd()
 
-	if not previous_position.buf then
-		print("No previous position saved!")
-		local oil_ok, oil = pcall(require, "oil")
-		if oil_ok then
-			oil.open(current_working_dir)
-		else
-			vim.cmd("Ex " .. current_working_dir)
-		end
+  if not previous_position.buf then
+    print("No previous position saved!")
+    local oil_ok, oil = pcall(require, "oil")
+    if oil_ok then
+      oil.open(current_working_dir)
+    else
+      vim.cmd("Ex " .. current_working_dir)
+    end
 
-		return
-	end
+    return
+  end
 
-	vim.api.nvim_set_current_buf(previous_position.buf)
-	vim.api.nvim_set_current_win(previous_position.win)
-	vim.api.nvim_win_set_cursor(previous_position.win, previous_position.cursor)
+  vim.api.nvim_set_current_buf(previous_position.buf)
+  vim.api.nvim_set_current_win(previous_position.win)
+  vim.api.nvim_win_set_cursor(previous_position.win, previous_position.cursor)
 
-	previous_position = {}
+  previous_position = {}
 end
 
 function M.SearchNotes()
-	local telescope_ok, telescope = pcall(require, "telescope.builtin")
-	if telescope_ok then
-		telescope.find_files({ cwd = "$HOME/personal/notes" })
-	else
-		print("Telescope is not installed!")
-	end
+  local telescope_ok, telescope = pcall(require, "telescope.builtin")
+  if telescope_ok then
+    telescope.find_files({ cwd = "$HOME/personal/notes" })
+  else
+    print("Telescope is not installed!")
+  end
 end
 
 vim.api.nvim_create_user_command("OpenNotes", "lua require('notes').OpenNotes()", {})
